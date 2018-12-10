5480b7fe98b7f5e061228308cc8022e30dd1ea3c
hushin <nnnnot+github@gmail.com>
2018-12-10 22:36:20 +0900

    add fish files

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,25 @@
+balias t tig
+balias ta 'tig --all'
+
+
+function checkout-git-branch -d "Fuzzy-find and checkout a branch"
+  git branch | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
+  commandline -f repaint
+end
+
+function checkout-recent-git-branch -d "Fuzzy-find and checkout a branch"
+  git for-each-ref --format="%(refname)" --sort=-committerdate | \
+  grep -v HEAD | \
+  grep -v refs/tags | \
+  sed -e "s|^refs/heads/||" | \
+  sed -e "s|^refs/remotes/||" | \
+  string trim | \
+  fzf | \
+  read -l result; and git checkout -t "$result"
+  commandline -f repaint
+end
+
+function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
+  ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
+  commandline -f repaint
+end
