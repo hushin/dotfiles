e6bbb915bcb6c13e949a19193599f39732c9f8d2
hushin <nnnnot+github@gmail.com>
2019-08-13 16:01:32 +0900

    fix ssh link

--- etc/init/osx/40_after_sync_apps.sh
+++ etc/init/osx/40_after_sync_apps.sh
@@ -8,5 +8,5 @@ read -p "Hit enter: "
 mkdir -p ~/Documents/memo
 [ -e ~/Dropbox/memo/_posts ] && [ ! -L ~/Documents/memo ] && ln -s ~/Dropbox/memo/_posts ~/Documents/memo
 
-[ -e ~/Dropbox/ssh/ ] && [ ! -L ~/.ssh ] && ln -s ~/Dropbox/ssh/ ~/.ssh
+[ -e ~/Dropbox/env/ssh/ ] && [ ! -L ~/.ssh ] && ln -s ~/Dropbox/env/ssh/ ~/.ssh
 chmod 0600 ~/.ssh/id_rsa
