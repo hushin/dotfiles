5ec33df61693f5ea1ac40c80077522df01bbdedf
hushin <nnnnot+github@gmail.com>
2020-02-24 11:48:40 +0900

    update node

--- etc/init/osx/08_npm.sh
+++ etc/init/osx/08_npm.sh
@@ -8,8 +8,8 @@ export PATH=$HOME/.nodebrew/current/bin:$PATH
 echo "install node"
 mkdir -p ~/.nodebrew/src
 nodebrew selfupdate
-nodebrew install-binary v10.x
-nodebrew use v10.x
+nodebrew install-binary v12.x
+nodebrew use v12.x
 
 echo "install npm packages"
 npm install -g npm
