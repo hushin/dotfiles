6d0a7a818c25ccb99b43a1be88c655030dbfcb0a
hushin <nnnnot+github@gmail.com>
2020-05-25 22:31:00 +0900

    update sync

--- etc/init/osx/40_after_sync_apps.sh
+++ etc/init/osx/40_after_sync_apps.sh
@@ -6,9 +6,11 @@ echo "Dropbox の設定が終わったら実行してください"
 read -p "Hit enter: "
 
 mkdir -p ~/Documents/memo
-[ -e ~/Dropbox/memo/_posts ] && [ ! -L ~/Documents/memo ] && ln -s ~/Dropbox/memo/_posts ~/Documents/memo
+[ -e ~/Dropbox/memo/_posts ] && [ ! -L ~/Documents/memo/_posts ] && ln -s ~/Dropbox/memo/_posts ~/Documents/memo
 
 [ -e ~/Dropbox/env/ssh/ ] && [ ! -L ~/.ssh ] && ln -s ~/Dropbox/env/ssh/ ~/.ssh
 chmod 0600 ~/.ssh/id_rsa
 
 [ -f ~/Dropbox/env/fish_history ] && [ ! -f ~/.local/share/fish/fish_history ] && ln -s ~/Dropbox/env/fish_history ~/.local/share/fish
+
+exit 0
