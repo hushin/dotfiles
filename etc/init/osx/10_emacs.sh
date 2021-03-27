70b3cf5d0e758ad45e7658716a7f4c65964386e5
hushin <nnnnot+github@gmail.com>
2021-03-27 13:46:55 +0900

    update emacs brew

--- etc/init/osx/10_emacs.sh
+++ etc/init/osx/10_emacs.sh
@@ -3,8 +3,8 @@
 set -u
 
 brew tap d12frosted/emacs-plus
-brew install emacs-plus
-# brew linkapps emacs-plus
+brew install emacs-plus@27 --with-spacemacs-icon
+brew link emacs-plus
 ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
 
 [ -e ~/.emacs.d ] || git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
