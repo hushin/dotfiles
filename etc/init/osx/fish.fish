e9b0d64477d10f43b9c28d21171806a8f3971c57
hushin <nnnnot+github@gmail.com>
2021-06-05 10:51:51 +0900

    fix fisher

--- etc/init/osx/fish.fish
+++ etc/init/osx/fish.fish
@@ -13,6 +13,6 @@ echo "install fisher"
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # install fisher packages
-fisher
+fisher update
 
 fish_update_completions
