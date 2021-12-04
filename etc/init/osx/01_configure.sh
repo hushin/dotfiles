1ac9dbed9656eb415a5d786c508d1189b78b0249
hushin <nnnnot+github@gmail.com>
2021-12-05 08:50:57 +0900

    update mac config

--- etc/init/osx/01_configure.sh
+++ etc/init/osx/01_configure.sh
@@ -34,7 +34,7 @@ defaults write NSGlobalDomain KeyRepeat -int 6
 defaults write NSGlobalDomain InitialKeyRepeat -int 35
 
 # サイドバーのアイコンサイズを大きく
-defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 3
+# defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 3
 
 # 拡張子を常に表示
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
@@ -113,7 +113,7 @@ defaults write com.apple.dock largesize -int 80
 defaults write com.apple.dock autohide -bool true
 
 # Wipe all app icons from the Dock （Dock に標準で入っている全てのアプリを消す、Finder とごみ箱は消えない）
-defaults write com.apple.dock persistent-apps -array
+# defaults write com.apple.dock persistent-apps -array
 
 # 最新の使用状況に基づいて操作スペースを自動的に並べ替えない
 defaults write com.apple.dock mru-spaces -bool false
