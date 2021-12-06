7ebe3c8eae34820bfeb8466ccb18ce46037e8e7b
hushin <ngtv.hsn@gmail.com>
2021-12-06 21:52:20 +0900

    fix fish config

--- home/.config/fish/keybinds.fish
+++ home/.config/fish/keybinds.fish
@@ -1,6 +1,6 @@
 # FIXME 動かなくなったので一時的に無効化
 # bind \x1b 'fzf_recentd' # Ctrl-[にバインドする
-bind \cj 'fzf_recentd'
+bind \cj 'FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --no-sort" fzf_recentd'
 bind \c] 'FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --preview \'bat --color=always --style=header,grid --line-range :80 (ghq root)/{}/README.*\'" fzf_ghq'
 bind \et 'FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --preview \'bat --color=always --style=header,grid --line-range :80 {}\'" __fzf_find_file'
 bind \cr '__fzf_reverse_isearch'
