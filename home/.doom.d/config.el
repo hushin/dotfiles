3fe95aac296949cda82cb1fcd1f851e806dc8b91
hushin <ngtv.hsn@gmail.com>
2023-03-29 21:36:30 +0900

    org-roam 本番へ

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -36,7 +36,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Dropbox/memo/org/")
-(setq org-roam-directory "~/Dropbox/memo/org/roam-sandbox")
+(setq org-roam-directory "~/Dropbox/memo/org/roam")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
