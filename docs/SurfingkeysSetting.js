3da05e8addb20533de5ddd660792ac702a987003
hushin <ngtv.hsn@gmail.com>
2024-02-11 19:44:28 +0900

    apply prettier

--- docs/SurfingkeysSetting.js
+++ docs/SurfingkeysSetting.js
@@ -18,16 +18,16 @@ const {
   Hints,
   Visual,
   RUNTIME,
-} = api
+} = api;
 
 // ---- Settings ----
-Hints.setCharacters('asdfgyuiowertnm')
-settings.scrollStepSize = 140
-settings.hintAlign = 'left'
+Hints.setCharacters('asdfgyuiowertnm');
+settings.scrollStepSize = 140;
+settings.hintAlign = 'left';
 // settings.aceKeybindings = 'emacs'
-settings.nextLinkRegex = /((forward|>>|next|次[のへ]|→)+)/i
-settings.prevLinkRegex = /((back|<<|prev(ious)?|前[のへ]|←)+)/i
-settings.historyMUOrder = false
+settings.nextLinkRegex = /((forward|>>|next|次[のへ]|→)+)/i;
+settings.prevLinkRegex = /((back|<<|prev(ious)?|前[のへ]|←)+)/i;
+settings.historyMUOrder = false;
 settings.theme = `
 #sk_status, #sk_find {
   font-size: 12pt;
@@ -35,11 +35,11 @@ settings.theme = `
 #sk_omnibarSearchResult .highlight {
   background: #f9ec89;
 }
-`
+`;
 
 // ---- Utils ----
-const unmapKeys = (keys) => keys.forEach((key) => unmap(key))
-const iunmapKeys = (keys) => keys.forEach((key) => iunmap(key))
+const unmapKeys = (keys) => keys.forEach((key) => unmap(key));
+const iunmapKeys = (keys) => keys.forEach((key) => iunmap(key));
 const escapeMap = {
   '&': '&amp;',
   '<': '&lt;',
@@ -49,15 +49,15 @@ const escapeMap = {
   '/': '&#x2F;',
   '`': '&#x60;',
   '=': '&#x3D;',
-}
+};
 const escapeForAlias = (str) =>
-  String(str).replace(/[&<>"'`=/]/g, (s) => escapeMap[s])
+  String(str).replace(/[&<>"'`=/]/g, (s) => escapeMap[s]);
 const createSuggestionItem = (html, props = {}) => {
-  const li = document.createElement('li')
-  li.innerHTML = html
-  return { html: li.outerHTML, props }
-}
-const padZero = (txt) => `0${txt}`.slice(-2)
+  const li = document.createElement('li');
+  li.innerHTML = html;
+  return { html: li.outerHTML, props };
+};
+const padZero = (txt) => `0${txt}`.slice(-2);
 const formatDate = (date, format = 'YYYY/MM/DD hh:mm:ss') =>
   format
     .replace('YYYY', date.getFullYear())
@@ -65,7 +65,7 @@ const formatDate = (date, format = 'YYYY/MM/DD hh:mm:ss') =>
     .replace('DD', padZero(date.getDate()))
     .replace('hh', padZero(date.getHours()))
     .replace('mm', padZero(date.getMinutes()))
-    .replace('ss', padZero(date.getSeconds()))
+    .replace('ss', padZero(date.getSeconds()));
 
 const tabOpenBackground = (url) =>
   RUNTIME('openLink', {
@@ -74,24 +74,24 @@ const tabOpenBackground = (url) =>
       active: false,
     },
     url,
-  })
+  });
 
 // ---- Maps ----
-map('>_r', 'r')
-map('>_E', 'E')
-map('>_R', 'R')
-map('>_S', 'S')
-map('S', 'sg') // 選択したテキストまたはクリップボードからググる
-map('r', 'gf') // 別タブで開く
-map('H', '>_S') // back in history
-map('L', 'D') // forward in history
-map('h', 'E') // previousTab
-map('l', '>_R') // nextTab
-map('R', '>_r') // reload
+map('>_r', 'r');
+map('>_E', 'E');
+map('>_R', 'R');
+map('>_S', 'S');
+map('S', 'sg'); // 選択したテキストまたはクリップボードからググる
+map('r', 'gf'); // 別タブで開く
+map('H', '>_S'); // back in history
+map('L', 'D'); // forward in history
+map('h', 'E'); // previousTab
+map('l', '>_R'); // nextTab
+map('R', '>_r'); // reload
 
-map('<Ctrl-[>', '<Esc>')
+map('<Ctrl-[>', '<Esc>');
 
-iunmap(':') // disable emoji
+iunmap(':'); // disable emoji
 // disable vim binding in insert mode
 iunmapKeys([
   '<Ctrl-a>',
@@ -100,16 +100,16 @@ iunmapKeys([
   '<Ctrl-b>',
   '<Ctrl-k>',
   '<Ctrl-y>',
-])
+]);
 // disable proxy
-unmapKeys(['cp', ';pa', ';pb', ';pc', ';pd', ';ps', ';ap'])
+unmapKeys(['cp', ';pa', ';pb', ';pc', ';pd', ';ps', ';ap']);
 
 // ---- Search ----
-removeSearchAlias('b')
-removeSearchAlias('w')
+removeSearchAlias('b');
+removeSearchAlias('w');
 
 // hatena tag
-addSearchAlias('th', 'hatena tag', 'http://b.hatena.ne.jp/search/tag?q=')
+addSearchAlias('th', 'hatena tag', 'http://b.hatena.ne.jp/search/tag?q=');
 
 // Twitter
 addSearchAlias(
@@ -124,40 +124,40 @@ addSearchAlias(
         url: `https://twitter.com/search?q=${encodeURIComponent(v.topic)}`,
       })
     )
-)
+);
 mapkey('otw', '#8Open Search with alias tw', function () {
-  Front.openOmnibar({ type: 'SearchEngine', extra: 'tw' })
-})
+  Front.openOmnibar({ type: 'SearchEngine', extra: 'tw' });
+});
 
 addSearchAlias(
   'tf',
   'Twitter フォロワーのみ',
   'https://twitter.com/search?pf=on&q='
-)
+);
 
 // Google jp 3ヶ月以内
 addSearchAlias(
   '3',
   'Google 3ヶ月以内',
   'https://www.google.co.jp/search?q={0}&tbs=qdr:m3,lr:lang_1ja&lr=lang_ja'
-)
+);
 
 // Yahoo!リアルタイム検索
 addSearchAlias(
   'r',
   'Yahoo!リアルタイム検索',
   'http://realtime.search.yahoo.co.jp/search?ei=UTF-8&p='
-)
+);
 mapkey('or', '#8Open Search with alias r', function () {
-  Front.openOmnibar({ type: 'SearchEngine', extra: 'r' })
-})
+  Front.openOmnibar({ type: 'SearchEngine', extra: 'r' });
+});
 
 // Wikipedia jp
 addSearchAlias(
   'wi',
   'Wikipedia',
   'https://ja.wikipedia.org/w/index.php?search='
-)
+);
 
 // MDN
 addSearchAlias(
@@ -167,7 +167,7 @@ addSearchAlias(
   's',
   'https://developer.mozilla.org/api/v1/search/ja?q=',
   (response) => {
-    const res = JSON.parse(response.text)
+    const res = JSON.parse(response.text);
     return res.documents.map((s) =>
       createSuggestionItem(
         `
@@ -179,9 +179,9 @@ addSearchAlias(
     `,
         { url: `https://developer.mozilla.org/${s.locale}/docs/${s.slug}` }
       )
-    )
+    );
   }
-)
+);
 
 // npm
 addSearchAlias(
@@ -192,23 +192,23 @@ addSearchAlias(
   'https://api.npms.io/v2/search/suggestions?size=20&q=',
   (response) =>
     JSON.parse(response.text).map((s) => {
-      let flags = ''
-      let desc = ''
-      let stars = ''
-      let score = ''
+      let flags = '';
+      let desc = '';
+      let stars = '';
+      let score = '';
       if (s.package.description) {
-        desc = escapeForAlias(s.package.description)
+        desc = escapeForAlias(s.package.description);
       }
       if (s.score && s.score.final) {
-        score = Math.round(Number(s.score.final) * 5)
-        stars = '⭐'.repeat(score) + '☆'.repeat(5 - score)
+        score = Math.round(Number(s.score.final) * 5);
+        stars = '⭐'.repeat(score) + '☆'.repeat(5 - score);
       }
       if (s.flags) {
         Object.keys(s.flags).forEach((f) => {
           flags += `[<span style='color:#ff4d00'>⚑</span> ${escapeForAlias(
             f
-          )}] `
-        })
+          )}] `;
+        });
       }
       return createSuggestionItem(
         `
@@ -223,9 +223,9 @@ addSearchAlias(
       </div>
     `,
         { url: s.package.links.npm }
-      )
+      );
     })
-)
+);
 
 // Amazon jp
 addSearchAlias(
@@ -235,7 +235,7 @@ addSearchAlias(
   's',
   'https://completion.amazon.co.jp/search/complete?method=completion&search-alias=aps&mkt=6&q=',
   (response) => JSON.parse(response.text)[1]
-)
+);
 
 // Amazon jp Kindle
 addSearchAlias(
@@ -245,89 +245,91 @@ addSearchAlias(
   's',
   'https://completion.amazon.co.jp/search/complete?method=completion&search-alias=aps&mkt=6&q=',
   (response) => JSON.parse(response.text)[1]
-)
+);
 
 // alc
-addSearchAlias('alc', 'alc', 'https://eow.alc.co.jp/search?q=')
+addSearchAlias('alc', 'alc', 'https://eow.alc.co.jp/search?q=');
 mapkey('oa', '#8Open Search with alias a', function () {
-  Front.openOmnibar({ type: 'SearchEngine', extra: 'alc' })
-})
+  Front.openOmnibar({ type: 'SearchEngine', extra: 'alc' });
+});
 
 // mercari
-addSearchAlias('me', 'mercari', 'https://www.mercari.com/jp/search/?keyword=')
+addSearchAlias('me', 'mercari', 'https://www.mercari.com/jp/search/?keyword=');
 
 // ---- Mapkeys ----//
 const copyTitleAndUrl = (format) => {
   const text = format
     .replace('%URL%', location.href)
-    .replace('%TITLE%', document.title)
-  Clipboard.write(text)
-}
+    .replace('%TITLE%', document.title);
+  Clipboard.write(text);
+};
 const copyHtmlLink = () => {
-  const clipNode = document.createElement('a')
-  const range = document.createRange()
-  const sel = window.getSelection()
-  clipNode.setAttribute('href', location.href)
-  clipNode.innerText = document.title
-  document.body.appendChild(clipNode)
-  range.selectNode(clipNode)
-  sel.removeAllRanges()
-  sel.addRange(range)
-  document.execCommand('copy', false, null)
-  document.body.removeChild(clipNode)
-  Front.showBanner('Ritch Copied: ' + document.title)
-}
+  const clipNode = document.createElement('a');
+  const range = document.createRange();
+  const sel = window.getSelection();
+  clipNode.setAttribute('href', location.href);
+  clipNode.innerText = document.title;
+  document.body.appendChild(clipNode);
+  range.selectNode(clipNode);
+  sel.removeAllRanges();
+  sel.addRange(range);
+  document.execCommand('copy', false, null);
+  document.body.removeChild(clipNode);
+  Front.showBanner('Ritch Copied: ' + document.title);
+};
 
 mapkey('cm', '#7Copy title and link to markdown', () => {
-  copyTitleAndUrl('[%TITLE%](%URL%)')
-})
+  copyTitleAndUrl('[%TITLE%](%URL%)');
+});
 mapkey('cM', '#7Copy all tabs to markdown', () => {
   RUNTIME('getTabs', { queryInfo: { currentWindow: true } }, (response) => {
     const tabUrls = [{ url: location.href, title: document.title }]
       .concat(response.tabs)
       .map((tab) => `- [${tab.title}](${tab.url})`)
-      .join('\n')
+      .join('\n');
 
-    Clipboard.write(tabUrls)
-  })
-})
+    Clipboard.write(tabUrls);
+  });
+});
 mapkey('ct', '#7Copy title and link to textile', () => {
-  copyTitleAndUrl('"%TITLE%":%URL%')
-})
+  copyTitleAndUrl('"%TITLE%":%URL%');
+});
 mapkey('ch', '#7Copy title and link to human readable', () => {
-  copyTitleAndUrl('%TITLE% / %URL%')
-})
+  copyTitleAndUrl('%TITLE% / %URL%');
+});
 mapkey('cb', '#7Copy title and link to scrapbox', () => {
-  copyTitleAndUrl('[%TITLE% %URL%]')
-})
+  copyTitleAndUrl('[%TITLE% %URL%]');
+});
 mapkey('ca', '#7Copy title and link to href', () => {
-  copyTitleAndUrl('<a href="%URL%">%TITLE%</a>')
-})
+  copyTitleAndUrl('<a href="%URL%">%TITLE%</a>');
+});
 mapkey('cp', '#7Copy title and link to plantuml', () => {
-  copyTitleAndUrl('[[%URL% %TITLE%]]')
-})
+  copyTitleAndUrl('[[%URL% %TITLE%]]');
+});
 mapkey('cr', '#7Copy rich text link', () => {
-  copyHtmlLink()
-})
+  copyHtmlLink();
+});
 mapkey('co', '#7Copy title and link to org', () => {
-  copyTitleAndUrl('[[%URL%][%TITLE%]]')
-})
+  copyTitleAndUrl('[[%URL%][%TITLE%]]');
+});
 mapkey('cO', '#7Copy all tabs to org', () => {
   RUNTIME('getTabs', { queryInfo: { currentWindow: true } }, (response) => {
     const tabUrls = [{ url: location.href, title: document.title }]
       .concat(response.tabs)
       .map((tab) => `- [[${tab.url}][${tab.title}]]`)
-      .join('\n')
+      .join('\n');
 
-    Clipboard.write(tabUrls)
-  })
-})
+    Clipboard.write(tabUrls);
+  });
+});
 //  8: Omnibar
 //  9: Visual Mode
 // 10: vim-like marks
 // 11: Settings
 // 12: Chrome URLs
-mapkey('gS', '#12Open Chrome settings', () => tabOpenLink('chrome://settings/'))
+mapkey('gS', '#12Open Chrome settings', () =>
+  tabOpenLink('chrome://settings/')
+);
 // 13: Proxy
 // 14: Misc
 mapkey(';a', '#14Save to Instapaper', () => {
@@ -335,10 +337,10 @@ mapkey(';a', '#14Save to Instapaper', () => {
     var d = document,
       z = d.createElement('scr' + 'ipt'),
       b = d.body,
-      l = d.location
+      l = d.location;
     try {
-      if (!b) throw 0
-      d.title = '(Saving...) ' + d.title
+      if (!b) throw 0;
+      d.title = '(Saving...) ' + d.title;
       z.setAttribute(
         'src',
         l.protocol +
@@ -346,105 +348,105 @@ mapkey(';a', '#14Save to Instapaper', () => {
           encodeURIComponent(l.href) +
           '&t=' +
           new Date().getTime()
-      )
-      b.appendChild(z)
+      );
+      b.appendChild(z);
     } catch (e) {
-      alert('Please wait until the page has loaded.')
+      alert('Please wait until the page has loaded.');
     }
   }
-  iprl5()
-})
+  iprl5();
+});
 
-unmap(';t')
+unmap(';t');
 mapkey(';t', '#14google translate', () => {
-  const selection = window.getSelection().toString()
+  const selection = window.getSelection().toString();
   if (selection === '') {
     // 文字列選択してない場合はページ自体を翻訳にかける
     tabOpenLink(
       `https://translate.google.com/translate?js=n&sl=auto&tl=ja&u=${window.location.href}`
-    )
+    );
   } else {
     // 選択している場合はそれを翻訳する
     tabOpenLink(
       `https://translate.google.com/?sl=auto&tl=ja&text=${encodeURI(selection)}`
-    )
+    );
   }
-})
+});
 
 mapkey(';b', '#14hatena bookmark', () => {
-  const { location } = window
-  let url = location.href
+  const { location } = window;
+  let url = location.href;
   if (location.href.startsWith('https://app.getpocket.com/read/')) {
     url = decodeURIComponent(
       document
         .querySelector('header a')
         .getAttribute('href')
         .replace('https://getpocket.com/redirect?url=', '')
-    )
+    );
   }
   if (url.startsWith('http:')) {
     tabOpenBackground(
       `http://b.hatena.ne.jp/entry/${url.replace('http://', '')}`
-    )
-    return
+    );
+    return;
   }
   if (url.startsWith('https:')) {
     tabOpenBackground(
       `http://b.hatena.ne.jp/entry/s/${url.replace('https://', '')}`
-    )
-    return
+    );
+    return;
   }
-  throw new Error('はてなブックマークに対応していないページ')
-})
+  throw new Error('はてなブックマークに対応していないページ');
+});
 
 mapkey(';G', '#14魚拓', () => {
-  tabOpenLink(`https://megalodon.jp/?url=${location.href}`)
-})
+  tabOpenLink(`https://megalodon.jp/?url=${location.href}`);
+});
 
 // org
-const escapeChars = ['(', ')', "'"]
+const escapeChars = ['(', ')', "'"];
 const escapeForOrg = (text) => {
-  let _text = text
+  let _text = text;
   escapeChars.forEach((char) => {
-    _text = _text.replaceAll(char, escape(char))
-  })
-  return _text
-}
+    _text = _text.replaceAll(char, escape(char));
+  });
+  return _text;
+};
 const getUrl = (path, query) => {
   const queryString = Object.entries(query)
     .map(
       ([key, value]) =>
         `${encodeURIComponent(key)}=${escapeForOrg(encodeURIComponent(value))}`
     )
-    .join('&')
-  return [path, queryString].join('?')
-}
+    .join('&');
+  return [path, queryString].join('?');
+};
 const orgCapture = (template) => {
   const url = getUrl('org-protocol://capture', {
     template,
     url: window.location.href,
     title: document.title.replace(/\|/g, '-'),
     body: window.getSelection(),
-  })
-  console.log(`orgCapture: ${url}`)
-  window.location.href = url
-}
+  });
+  console.log(`orgCapture: ${url}`);
+  window.location.href = url;
+};
 mapkey(',m', '#14org-capture memo', () => {
-  orgCapture('M')
-})
+  orgCapture('M');
+});
 mapkey(',t', '#14org-capture todo', () => {
-  orgCapture('T')
-})
+  orgCapture('T');
+});
 mapkey(',l', '#14org-capture read it later', () => {
-  orgCapture('L')
-})
+  orgCapture('L');
+});
 
 mapkey('=q', '#14Delete query', () => {
-  location.href = location.href.replace(/\?.*/, '')
-})
+  location.href = location.href.replace(/\?.*/, '');
+});
 mapkey('=h', '#14Delete hash', () => {
-  location.href = location.href.replace(/\#.*/, '')
-})
+  location.href = location.href.replace(/\#.*/, '');
+});
 // 15: Insert Mode
 
 // ---- qmarks ----
@@ -453,44 +455,44 @@ const qmarksMapKey = (prefix, urls, newTab) => {
     RUNTIME('openLink', {
       tab: { tabbed: newTab },
       url: link,
-    })
-  }
+    });
+  };
   for (const key in urls) {
-    mapkey(prefix + key, `qmark: ${urls[key]}`, openLink(urls[key], newTab))
+    mapkey(prefix + key, `qmark: ${urls[key]}`, openLink(urls[key], newTab));
   }
-}
+};
 const qmarksUrls = {
   m: 'https://mail.google.com/mail/u/0/',
   h: 'http://b.hatena.ne.jp/hush_in/hotentry',
   i: 'https://www.instapaper.com/u',
   tw: 'https://twitter.com/',
   td: 'https://tweetdeck.twitter.com/',
-}
-unmap('gn')
-qmarksMapKey('gn', qmarksUrls, true)
-qmarksMapKey('gO', qmarksUrls, false)
+};
+unmap('gn');
+qmarksMapKey('gn', qmarksUrls, true);
+qmarksMapKey('gO', qmarksUrls, false);
 
 // --- Site-specific mappings ---
-const clickElm = (selector) => () => document.querySelector(selector).click()
+const clickElm = (selector) => () => document.querySelector(selector).click();
 if (/speakerdeck.com/.test(window.location.hostname)) {
   const clickElmFr = (selector) => () =>
     document
       .querySelector('.speakerdeck-iframe')
       .contentWindow.document.querySelector(selector)
-      .click()
-  mapkey(']', 'next page', clickElmFr('.sd-player-next'))
-  mapkey('[', 'prev page', clickElmFr('.sd-player-previous'))
+      .click();
+  mapkey(']', 'next page', clickElmFr('.sd-player-next'));
+  mapkey('[', 'prev page', clickElmFr('.sd-player-previous'));
 }
 
 if (/www.slideshare.net/.test(window.location.hostname)) {
-  mapkey(']', 'next page', clickElm('#btnNext'))
-  mapkey('[', 'prev page', clickElm('#btnPrevious'))
+  mapkey(']', 'next page', clickElm('#btnNext'));
+  mapkey('[', 'prev page', clickElm('#btnPrevious'));
 }
 
 if (/booklog.jp/.test(window.location.hostname)) {
-  mapkey(']', 'next page', clickElm('#modal-review-next'))
-  mapkey('[', 'prev page', clickElm('#modal-review-prev'))
-  mapkey('d', '読み終わった', clickElm('#status3'))
+  mapkey(']', 'next page', clickElm('#modal-review-next'));
+  mapkey('[', 'prev page', clickElm('#modal-review-prev'));
+  mapkey('d', '読み終わった', clickElm('#status3'));
   mapkey('R', 'Read by Kindle', () =>
     RUNTIME('openLink', {
       tab: { tabbed: true },
@@ -499,34 +501,34 @@ if (/booklog.jp/.test(window.location.hostname)) {
         .getAttribute('href')
         .replace(/.*\//, '')}`,
     })
-  )
+  );
 }
 
 if (/www.amazon.co.jp/.test(window.location.hostname)) {
   mapkey('=s', '#14URLを短縮', () => {
     location.href = `https://www.amazon.co.jp/dp/${
       document.querySelectorAll("[name='ASIN'], [name='ASIN.0']")[0].value
-    }`
-  })
+    }`;
+  });
 }
 
 if (
   /^https:\/\/b.hatena.ne.jp\/.*\/hotentry\?date/.test(window.location.href)
 ) {
   const moveDate = (diff) => () => {
-    const url = new URL(window.location.href)
-    const dateTxt = url.searchParams.get('date')
-    const [_, yyyy, mm, dd] = dateTxt.match(/(....)(..)(..)/)
+    const url = new URL(window.location.href);
+    const dateTxt = url.searchParams.get('date');
+    const [_, yyyy, mm, dd] = dateTxt.match(/(....)(..)(..)/);
     const date = new Date(
       parseInt(yyyy, 10),
       parseInt(mm, 10) - 1,
       parseInt(dd, 10) + diff
-    )
-    url.searchParams.set('date', formatDate(date, 'YYYYMMDD'))
-    location.href = url.href
-  }
-  mapkey(']]', 'next date', moveDate(1))
-  mapkey('[[', 'prev date', moveDate(-1))
+    );
+    url.searchParams.set('date', formatDate(date, 'YYYYMMDD'));
+    location.href = url.href;
+  };
+  mapkey(']]', 'next date', moveDate(1));
+  mapkey('[[', 'prev date', moveDate(-1));
 }
 
 if (/youtube.com/.test(window.location.hostname)) {
@@ -534,12 +536,12 @@ if (/youtube.com/.test(window.location.hostname)) {
     'F',
     'Toggle fullscreen',
     clickElm('.ytp-fullscreen-button.ytp-button')
-  )
+  );
   mapkey(
     'gH',
     'GoTo Home',
     () => (location.href = 'https://www.youtube.com/feed/subscriptions?flow=2')
-  )
+  );
 }
 
 if (
@@ -565,11 +567,11 @@ if (
     'ch',
     ',t',
     ',m',
-  ])
+  ]);
 }
 if (/^https:\/\/www.amazon.co.jp\/gp\/video\//.test(window.location.href)) {
   // for Video Speed Controller
-  unmapKeys(['d', 's', 'z', 'x', 'r', 'g'])
+  unmapKeys(['d', 's', 'z', 'x', 'r', 'g']);
 }
 
 // click `Save` button to make above settings to take effect.
